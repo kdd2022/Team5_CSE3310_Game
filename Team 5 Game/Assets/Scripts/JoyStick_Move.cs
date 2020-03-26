70c2199bec8ff134644322a3510c3ccec5802d55
ColinK99 <colin.kirch99@yahoo.com>
2020-03-26 18:26:28 -0500

    Unity w/ Test Assets
    Added two test assets, for the player and impassable objects. A tileset has been detected and is subject to change as more information is gathered

---
+++ Team 5 Game/Assets/Scripts/JoyStick_Move.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStick_Move : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    private bool Pressed;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
