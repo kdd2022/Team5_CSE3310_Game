8011ec1ff07795f41f257149dd46bfa61bc3c379
kdd2022 <61127486+kdd2022@users.noreply.github.com>
2020-04-12 14:30:25 -0500

    Version 1 of enemy ai
    Testing Enemy AI

---
+++ Game_Kyle/Assets/Scripts/myDamage.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//This script is used to set the attack value of the player
+public class myDamage : MonoBehaviour
+{
+    //This value can be adjusted under the AttackBox object under the Player Object
+    //The value will be used in the EnemyScript
+    public int realDamage;
+}
